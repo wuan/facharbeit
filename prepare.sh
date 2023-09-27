bcdafea858061dc9185424119907431f699e098a
Andreas Würl <andi@tryb.de>
2023-09-27 22:45:57 +0200

    fix installation

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apk add fig2dev --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community
+apk add fig2dev --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
 
 fig2dev -L epic skizze1.fig skizze1.tex
 fig2dev -L epic skizze2.fig skizze2.tex
