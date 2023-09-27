f675eb7bf42e08dd6074783824fcbba59cae7764
Andreas Würl <andi@tryb.de>
2023-09-27 22:27:16 +0200

    update build

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apk add fig2dev
+apk add fig2dev --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community
 
 fig2dev -L epic skizze1.fig skizze1.tex
 fig2dev -L epic skizze2.fig skizze2.tex
