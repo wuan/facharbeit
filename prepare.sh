e509775f9399910c2381f81aeec0908fd339e05b
Andreas Würl <andi@tryb.de>
2023-09-27 23:41:44 +0200

    fix graphics

--- prepare.sh
+++ prepare.sh
@@ -3,6 +3,6 @@
 apk add gnuplot
 apk add fig2dev --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/
 
-fig2dev -L epic skizze1.fig skizze1.tex
-fig2dev -L epic skizze2.fig skizze2.tex
+fig2dev -L latex skizze1.fig skizze1.tex
+fig2dev -L latex skizze2.fig skizze2.tex
 gnuplot funktion1.gnu
