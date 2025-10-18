d82a6789e81d9d9d0a5a9d288b2aad21435cd164
Andreas Würl <andi@tryb.de>
2025-10-18 07:37:21 +0200

    cleanup build

--- prepare.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apk add fig2dev --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community/
-
-fig2dev -L latex skizze1.fig skizze1.tex
-fig2dev -L latex skizze2.fig skizze2.tex
-gnuplot funktion1.gnu
